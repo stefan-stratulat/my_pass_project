abd0a0a6b10eb9d5bd05ec4811663a883c424fd4
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-03-08 13:14:03 +0200

    added search functionality

--- main.py
+++ main.py
@@ -4,6 +4,26 @@ from random import choice,randint,shuffle
 import pyperclip
 import json
 
+# ---------------------------- SEARCH DATA ------------------------------- #
+
+def search_data():
+    website_data = website_input.get()
+    try:
+        with open('data.json','r') as file:
+            #Read old data
+            data = json.load(file)
+    #error could happpen at first use if file not created
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="File not found. Try adding data first!")
+    else:
+        if website_data in data:
+                email = data[website_data]["email"]
+                password = data[website_data]["password"]
+                messagebox.showinfo(title="Password info",
+                message=f"{website_data} info:\n Email:{email}\nPassword:{password}")
+        else:
+            messagebox.showerror(title="Error", message=f"Not details for {website_data}.")
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 def generate_password():
@@ -59,9 +79,9 @@ def save_data():
         else:
             with open('data.json', "w") as file:
                 json.dump(data, file, indent=4)
-
-        website_input.delete(0, END)
-        password_input.delete(0, END)
+        finally:
+            website_input.delete(0, END)
+            password_input.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -82,7 +102,7 @@ email_label = Label(text="Email/Username")
 password_label = Label(text="Password")
 
 #ENTRY WIDGETS
-website_input = Entry(width=40)
+website_input = Entry(width=22)
 website_input.focus()
 email_input = Entry(width=40)
 email_input.insert(0,"example@gmail.com")
@@ -91,6 +111,7 @@ password_input = Entry(width=22)
 #BUTTONS
 password_button = Button(text="Generate Password",command=generate_password)
 add_button = Button(text="Add",width=35,command=save_data)
+search_button = Button(text="Search",width=15,command=search_data)
 
 #GRID
 #col 0
@@ -99,11 +120,12 @@ email_label.grid(row=2,column=0)
 password_label.grid(row=3,column=0)
 #col 1
 canvas.grid(row=0,column=1)
-website_input.grid(row=1,column=1,columnspan=2)
+website_input.grid(row=1,column=1)
 email_input.grid(row=2,column=1,columnspan=2)
 password_input.grid(row=3,column=1)
 add_button.grid(row=4,column=1,columnspan=2)
 #col 2
 password_button.grid(row=3,column=2)
+search_button.grid(row=1,column=2)
 
 window.mainloop()
